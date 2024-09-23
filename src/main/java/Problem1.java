fef921698e83a268ecbe65bad823913e124d533b
Jordan Neff <JN1024482@wcupa.edu>
2024-09-22 20:51:23 -0400

    first attempt at problem 2

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,6 +4,11 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+
+
+
+
+
+        return l1;
     }
 }
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,9 +1,28 @@
+import java.util.List;
 
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
         // TODO: Implement the insert method
         // This method should insert a new node with the given value at the specified position
         // If the position is out of bounds, insert the node at the end of the list
+
+        if(position < 0) {
+            ListNode r = head;
+            while(r != null) {
+                r = r.next;
+            }
+            r.next = new ListNode(val);
+        } else if(position == 1 || head == null) {
+            head.next = new ListNode(val);
+        } else {
+            ListNode ref = head;
+            int index = 0;
+            while (index != position) {
+                index++;
+                ref = ref.next;
+            }
+            ref.next = new ListNode(val);
+        }
         return null;
     }
 }
