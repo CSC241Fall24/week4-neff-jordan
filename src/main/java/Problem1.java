782ced04ef30886eac640cebfa0f30275ef5b6a4
Jordan Neff <JN1024482@wcupa.edu>
2024-09-26 17:06:41 -0400

    maybe solution

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,6 +5,9 @@ public class Problem1 {
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
 
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
         ListNode ref = l1;
 
         //get to the end of l1 list
