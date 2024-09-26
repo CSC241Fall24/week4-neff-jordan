71ccface7e52c0c1ae0f28c8812e92f4ba789cb4
Jordan Neff <JN1024482@wcupa.edu>
2024-09-26 17:01:26 -0400

    maybe solution

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,8 +5,20 @@ public class Problem1 {
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
 
+        ListNode ref = l1;
 
+        //get to the end of l1 list
+        while(ref.next != null) {
+            ref = ref.next;
+        }
 
+        while(l2 != null) {
+            int tempVal = ref.val;
+            ListNode newNode = new ListNode(tempVal);
+            ref.next = newNode;
+            ref = ref.next;
+            l2 = l2.next;
+        }
 
 
         return l1;
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -6,13 +6,11 @@ public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
         ListNode newNode = new ListNode(val);
 
-        // Insert at the beginning or if the list is empty
         if (head == null || position <= 1) {
             newNode.next = head;
-            return newNode; // New node becomes the head
+            return newNode;
         }
 
-        // Insert at the end if the position is out of bounds
         else if (position > size(head)) {
             ListNode ref = head;
             while (ref.next != null) {  // Traverse to the last node
@@ -21,26 +19,20 @@ public class Problem2 {
             ref.next = newNode;
         }
 
-        // Insert in the middle at the specified position
         else {
+
             ListNode ref = head;
             int count = 1;
 
-            // Traverse until the node just before the position
             while (ref != null && count < position - 1) {
                 ref = ref.next;
                 count++;
             }
-
-            // Insert the new node
             newNode.next = ref.next;
             ref.next = newNode;
         }
-
         return head;
     }
-
-    // Method to calculate the size of the linked list
     public static int size(ListNode head) {
         ListNode ref = head;
         int count = 0;
