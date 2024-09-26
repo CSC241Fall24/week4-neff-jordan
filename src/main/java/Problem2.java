eb1646ae6ceffefcfde1b59635a07e8b2ed45a12
Jordan Neff <JN1024482@wcupa.edu>
2024-09-26 16:20:59 -0400

    new attempt

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Node;
+
 import java.util.List;
 
 public class Problem2 {
@@ -5,24 +7,39 @@ public class Problem2 {
         // TODO: Implement the insert method
         // This method should insert a new node with the given value at the specified position
         // If the position is out of bounds, insert the node at the end of the list
-
-        if(position < 0) {
-            ListNode r = head;
-            while(r != null) {
-                r = r.next;
-            }
-            r.next = new ListNode(val);
-        } else if(position == 1 || head == null) {
-            head.next = new ListNode(val);
-        } else {
+        if(head == null && position == 1) head.next = new ListNode(val);
+        else if(position < 0 || position > size(head)) {
             ListNode ref = head;
-            int index = 0;
-            while (index != position) {
-                index++;
+            while(ref != null) {
+                ref = ref.next;
+            }
+            ref.next = new ListNode(val);
+        } else {
+            int count = 1;
+            ListNode ref = head;
+            while(count != position) {
+                count++;
                 ref = ref.next;
             }
             ref.next = new ListNode(val);
         }
         return head;
     }
+    public static int size(ListNode head) {
+        ListNode ref = head;
+        int count = 0;
+        while(ref != null) {
+            count++;
+            ref = ref.next;
+        }
+        return count;
+    }
 }
+
+/*
+    Implement a function to insert a new node with a given value at a specified position in a linked list
+
+ * Constraints:
+ * – If the position is out of bounds, insert the node at the end of the list
+ * – If the position is 1 or the list is empty, insert at the beginning
+ */
